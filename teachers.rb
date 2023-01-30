3ddcd6b84a0d7275506758058ccc1b12342a73b2
Luis Muñoz <l.munoz.m@outlook.com>
2023-01-30 10:36:15 -0300

    Create teachers.rb
    Add Teacher class.

---
+++ teachers.rb
@@ -0,0 +1,12 @@
+require './persons'
+
+class Teacher < Person
+  def initialize(specialization)
+    super(name, age)
+    @specialization = specialization
+  end
+
+  def can_use_services?
+    true
+  end
+end
