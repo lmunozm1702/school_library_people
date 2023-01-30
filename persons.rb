8180ef43880d1e3227aa82009cf00deb673d239d
Luis Muñoz <l.munoz.m@outlook.com>
2023-01-30 11:35:38 -0300

    Fix linter errors.
    persons.rb:5:41: C: Style/OptionalBooleanParameter: Prefer keyword arguments for arguments with a boolean default value; use parent_permission: true instead of parent_permission = true.
      def initialize(name, age = 'unknown', parent_permission = true)
                                            ^^^^^^^^^^^^^^^^^^^^^^^^
    persons.rb:11:7: C: Naming/PredicateName: Rename is_of_age? to of_age?.
      def is_of_age?

--- persons.rb
+++ persons.rb
@@ -2,19 +2,19 @@ class Person
   # creating accessors
   attr_accessor :name, :age, :id
 
-  def initialize(name, age = 'unknown', parent_permission = true)
+  def initialize(name, age = 'unknown', parent_permission: true)
     @name = name
     @age = age
     @parent_permission = parent_permission
   end
 
-  def is_of_age?
+  def of_age?
     return true if @age >= 18
 
     false
   end
 
-  private :is_of_age?
+  private :of_age?
 
   def can_use_services?
     return true if is_of_age? || @parent_permission
@@ -22,3 +22,6 @@ class Person
     false
   end
 end
+
+a = Person.new('Benja')
+pp a
